1fe2b156027680bd5be3bdd1683457288c798d62
Geonwoo Kim <geonwoo@Geonwoos-Macbook-Air.local>
2023-03-11 18:39:47 -0800

    update

--- src/index.js
+++ src/index.js
@@ -32,3 +32,14 @@ app.on('activate', () => {
     createWindow();
   }
 });
+
+function createBrowserWindow() {
+  const remote = require('electron').remote;
+  const BrowserWindow = remote.BrowserWindow;
+  const win = new BrowserWindow({
+    height: 600,
+    width: 800
+  });
+
+  win.loadURL('search-page.html');
+}
