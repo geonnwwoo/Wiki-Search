b4259a1c5d2b29d28000364ed0a6f3c975c9432f
Geonwoo Kim <geonwoo@Geonwoos-Macbook-Air.local>
2023-03-11 16:52:11 -0800

    updated start page render javascript file to console log searchval

--- src/start-page-render.js
+++ src/start-page-render.js
@@ -1,12 +1,15 @@
 // Move to Search Results Page
 
-document.querySelector("#aa1").addEventListener('keypress', function(e) {
+function checkEnter(e) {
     if (e.key == 'Enter' && document.querySelector('#aa2').value.length != 0) {
         searched();
     }
-});
+}
+
+document.querySelector("#aa1").addEventListener('keypress', checkEnter, false);
 
 function searched() {
     let searchVal = document.querySelector("#aa2").value;
     console.log(searchVal);
+    document.removeEventListener('keypress', checkEnter);
 }
