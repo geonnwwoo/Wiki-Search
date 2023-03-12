1b6b4382ac6cd86717a2467d2f1e127cad72024d
Geonwoo Kim <geonwoo@Geonwoos-Macbook-Air.local>
2023-03-11 16:28:53 -0800

    added search text input in start page

--- src/start-page-render.js
+++ src/start-page-render.js
@@ -1,5 +1,12 @@
+// Move to Search Results Page
+
 document.querySelector("#aa1").addEventListener('keypress', function(e) {
-    if (e.key == 'Enter') {
-        console.log("Enter Pressed");
+    if (e.key == 'Enter' && document.querySelector('#aa2').value.length != 0) {
+        searched();
     }
 });
+
+function searched() {
+    let searchVal = document.querySelector("#aa2").value;
+    console.log(searchVal);
+}
