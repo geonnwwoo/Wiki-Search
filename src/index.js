c06521df31c1312afedb08005220ec9efeccdd55
Geonwoo Kim <geonwoo@Geonwoos-Macbook-Air.local>
2023-03-12 19:26:57 -0700

    romoved unnecessary child window

--- src/index.js
+++ src/index.js
@@ -35,28 +35,3 @@ app.on('activate', () => {
 
 
 
-
-
-// Child Window
-
-const createChildWindow = () => {
-  const childWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
-    },
-  });
-
-  childWindow.loadFile(path.join(__dirname, 'search-page.html'));
-  childWindow.webContents.openDevTools();
-};
-
-app.on('ready', createChildWindow);
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
