5aaf1715c8caa0c9e1d71eca6512be74e56c6cdf
Geonwoo Kim <geonwoo@Geonwoos-Macbook-Air.local>
2023-03-12 19:26:19 -0700

    render js file to include enter button as enter input

--- src/render.js
+++ src/render.js
@@ -5,11 +5,19 @@ function checkEnter(e) {
         searched();
     }
 }
+function checkEnterButton() {
+    if (document.querySelector('#aa2').value.length != 0) {
+        searched()
+    }
+}
 
+document.querySelector('#aa3').addEventListener('click', checkEnterButton, false);
 document.querySelector("#aa1").addEventListener('keypress', checkEnter, false);
 
 function searched() {
     let searchVal = document.querySelector("#aa2").value;
     console.log(searchVal);
     document.querySelector("#aa1").removeEventListener('keypress', checkEnter);
+    document.querySelector('#aa3').removeEventListener('click', checkEnterButton)
 }
+
