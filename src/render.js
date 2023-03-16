de907dcd1ee7400cdbe454744530e7c05707342c
Geonwoo Kim <geonwoo@Geonwoos-Macbook-Air.local>
2023-03-15 18:24:03 -0700

    updates

--- src/render.js
+++ src/render.js
@@ -1,6 +1,7 @@
 // Move to Search Results Page
 
 let searchUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=';
+let contentUrl = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=';
 let userCardTemplate = document.querySelector("[search-results-template]");
 let userCardContainer = document.querySelector("[data-search-query-container]");
 
