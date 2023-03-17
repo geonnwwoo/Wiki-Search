55e93fe785481786a3d02307e46044e191708d43
Geonwoo Kim <geonwoo@Geonwoos-Macbook-Air.local>
2023-03-16 17:37:16 -0700

    formatting of html

--- src/render.js
+++ src/render.js
@@ -13,7 +13,7 @@ function getUrl(yourUrl){
 }
 
 function checkEnter(e) {
-    if (e.key == 'Enter' && document.querySelector('#aa2').value.length != 0) {
+    if (e.key == 'Enter' && document.querySelector('.search-bar-input').value.length != 0) {
         searched();
     }
 }
@@ -23,24 +23,33 @@ function checkEnterButton() {
     }
 }
 
-document.querySelector('#aa3').addEventListener('click', checkEnterButton, false);
-document.querySelector("#aa1").addEventListener('keypress', checkEnter, false);
+document.querySelector('.search-bar-enter').addEventListener('click', checkEnterButton, false);
+document.querySelector(".search-bar-wrapper").addEventListener('keypress', checkEnter, false);
 
 function searched() {
-    let searchVal = document.querySelector("#aa2").value;
+    let searchVal = document.querySelector(".search-bar-input").value;
     let url = searchUrl + searchVal;
     console.log(searchVal);
-    document.querySelector("#aa1").removeEventListener('keypress', checkEnter);
-    document.querySelector('#aa3').removeEventListener('click', checkEnterButton);
     gotData(JSON.parse(getUrl(url)));
 
     //location.href='search-page.html' ;
 }
 
 function gotData(data) {
-    console.log(data);
     console.log(data[1].length);
     let dataLength = data[1].length;
+
+    if (dataLength != 0) {
+        document.querySelector(".search-bar-wrapper").removeEventListener('keypress', checkEnter);
+        document.querySelector('.search-bar-enter').removeEventListener('click', checkEnterButton);
+        for (let i=0; i<dataLength; i++) {
+            let card = userCardTemplate.content.cloneNode(true).children[0] ;
+            let header = card.querySelector("[data-header]");
+            header.textContent = data[1][i];
+            userCardContainer.append(card);
+        }
+    }
+
     for (let i=0; i<dataLength; i++) {
         let card = userCardTemplate.content.cloneNode(true).children[0] ;
         let header = card.querySelector("[data-header]");
