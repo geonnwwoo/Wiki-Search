fc4f5e18767d02d971758c2bf3df5915b5b62a98
Geonwoo Kim <gwkim973@gmail.com>
2023-03-20 21:34:37 -0700

    added visual (colorscheme implementation into search page render js file)

--- src/search-page-render.js
+++ src/search-page-render.js
@@ -10,3 +10,33 @@ ipc.on('start->search: content article received', function(event, articleContent
 ipc.on('start->search: title received', function(event, articleTitle) {
     title.textContent = articleTitle;
 });
+
+
+
+// VISUAL (COLORSCHEMES)
+
+function readTextFile(file, callback) {
+    let rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+readTextFile("colorscheme/colorscheme.txt", function(text1) {
+    let colorscheme = text1;
+    readTextFile("colorscheme/"+colorscheme, function(text2) {
+        let colorschemeText = text2;
+        gotColorScheme(colorschemeText);
+    });
+});
+
+function gotColorScheme(cs) {
+    let colorschemeCSS = JSON.parse(cs);
+    let r = document.querySelector(':root');
+    r.style.setProperty('--body-color', colorschemeCSS["SEARCH-body"]);
+}
